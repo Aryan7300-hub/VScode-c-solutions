9a2fec234c3be18cd5f752e6b4648c393950a209
Aryan7300-hub <aryanjindal7300@gmail.com>
2026-08-27 23:57:21 +0530

    LeetCode Question

--- LC-1920_BuildFromPermution.c
+++ LC-1920_BuildFromPermution.c
@@ -2,10 +2,10 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* buildArray(int* nums, int numsSize, int* returnSize) {
-    int* arr = malloc(numsSize*sizeof(int));
+    int* result = malloc(numsSize*sizeof(int));
     for(int i = 0; i < numsSize; i++){
-        arr[i] = nums[nums[i]];
+        result[i] = nums[nums[i]];
     }
     *returnSize = numsSize;
-    return arr;
+    return result;
 }
